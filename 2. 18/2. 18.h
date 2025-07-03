1eef74b7b30acb62104d2957b9f427cde3985fd1
Igor Tsaponenko <igorTsapovf@proton.me>
2025-07-03 11:11:51 +0300

    Created new second task

--- 2. 18/2. 18.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <fstream>
-#include <sstream>
-
-bool isSymbol(char commentSymbol, std::string string);
-void parseFile(int** arr, std::string string);
-void parseString(int* arr, std::string string);
-void output();
